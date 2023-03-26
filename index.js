3c2835a31a4804513bf5fcc99c6cbf9824b7d9de
manojpakade <115654500+manojpakade123@users.noreply.github.com>
2023-03-26 20:34:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send({ hello: "world" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2000;
 app.listen(PORT, function() {
   console.log(`App listening on port ${PORT}`);
 });
